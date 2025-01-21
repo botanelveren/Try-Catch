c81c3130e4f3942b76737b645e428fcb77601710
Botan Elveren <botanelveren@gmail.com>
2025-01-21 18:45:15 +0300

    Add project files.

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            string girdi = Console.ReadLine();
+
+            try
+            {
+                int sayi = int.Parse(girdi);
+                Console.WriteLine("Sayının karesi: " + (sayi * sayi));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        }
+    }
+}
